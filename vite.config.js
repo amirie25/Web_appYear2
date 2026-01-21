0e194f4ebcc8a00b1c88b5bd95b6ed7323871186
Haru M <miriam.anchidin@emanuel.ro>
2026-01-22 00:24:59 +0200

    adding pwa

--- vite.config.js
+++ vite.config.js
@@ -11,28 +11,71 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      registerType: 'autoUpdate', devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Your App Name',
-        short_name: 'App',
-        description: 'Your app description',
-        theme_color: '#ffffff',
+        name: "Task List",
+        short_name: "Task List",
+        theme_color: "#1976d2",
+        background_color: "#fafafa",
+        display: "standalone",
+        scope: "./",
+        start_url: "./",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+            purpose: "maskable any"
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "icons/icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "maskable any"
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any"
           }
         ]
       }
-    })
-  ],
+    })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
